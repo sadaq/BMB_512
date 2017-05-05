37ae1840d34d0209f91db46ed4dee8b1da3b1a5e
sadaq <srmegh84@gmail.com>
2017-05-05 23:25:29 +0600

    update print and input

---
+++ Class 02/print_input.py
@@ -0,0 +1,11 @@
+# basic print
+print "hello world!"
+
+# print multiple
+print "hello ", "world"
+
+# numarical input
+inp1 = input("input number:")
+
+# string input
+inp2 = raw_input("input string: ")
