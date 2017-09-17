14fdbb613a2d26e1e1cd08bf780773c3c5234bb5
DESKTOP-2\sadaq <srmegh84@gmail.com>
2017-09-17 10:48:12 +0600

    Class 4 added

---
+++ Class 04/list.py
@@ -0,0 +1,42 @@
+'''
+all_marks = [65,51,75,67,59]
+first_mark = all_marks[0]
+print first_mark
+
+three_marks = all_marks[0:3]
+print three_marks
+
+ev_2nd_marks = all_marks[0:3:2]
+print ev_2nd_marks
+'''
+
+marks = [65, 67, 75, 40, 57]
+#print marks[0:3]
+#print marks[3]
+#print marks[2:]
+#print marks[:2]
+#print marks[:-1]
+#print marks[:]
+#print marks[0:4:1]
+
+
+#print max(marks)
+#print min(marks)
+#print len(marks)
+#print sorted(marks)
+#print sorted(marks, reverse = True)
+#print 56 in marks
+#print 56 not in marks
+marks.append(56)
+marks.append(70)
+#print marks
+
+
+more_marks = [71, 60, 69]
+#marks[0] = 70
+print marks
+new_marks = marks + more_marks
+print new_marks
+
+
+
