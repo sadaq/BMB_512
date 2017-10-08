472719681c6e7b027e8f541d9787533277bd9db0
DESKTOP-2\sadaq <srmegh84@gmail.com>
2017-10-08 09:44:27 +0600

    no message

---
+++ Class 13/Class 13.py
@@ -0,0 +1,41 @@
+'''
+#Dictonary Practice
+
+names = {'A':"Adenine",'C':"Cytosine",'G':"Guanine",'T':"Thymine",'U':"Urcile"}
+print names['A']
+#Update
+namess['U'] = "Others"
+#Delete
+del names['U']
+#Find key
+names.key()
+
+#Find values
+names.values()
+
+#Find items/pairs
+names.items()
+
+
+'''
+#reverse complement
+s = "ATACCA"        ##input sequence
+
+s1 = s[::-1]        ##Reverse the sequence
+s2 = ''
+
+for i in s1:        ##Access every base
+    if i == "A":    ##Check every base
+        i = "T"     ##Change every base
+        s2+=i       ##Add to new string
+    elif i == "T":
+        i = "A"
+        s2+=i
+    elif i == "G":
+        i = "C"
+        s2+=i
+    elif i == "C":
+        i = "G"
+        s2+=i
+
+print s2            ##Print complement
