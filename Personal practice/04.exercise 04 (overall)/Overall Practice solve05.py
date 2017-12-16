411bca68aebcfe90edcd25c1bd7d1e5fe5aa52d3
DESKTOP-2\sadaq <srmegh84@gmail.com>
2017-12-16 16:59:07 +0600

    no message

--- Personal practice/04.exercise 04 (overall)/Overall Practice solve05.py
+++ Personal practice/04.exercise 04 (overall)/Overall Practice solve05.py
@@ -1,11 +1,48 @@
 # FIles
 #2
-
+seq = ''
+with open("01.fasta", 'r') as fh:
+    lines0 = fh.readlines()
+    for line in lines0:
+        if not line.startswith(">"):
+            seq+= line
+            print line
 #3
+def GC(seq):
+    seq = seq.upper()
+    total = float(len(seq))
+    G = seq.count('G')
+    C = seq.count('C')
+    gc = ((G+C)*100)/total
+    return gc
+with open("02.fasta", 'r') as fh:
+    with open("02result.fasta", "w") as fh2:
+        lines0 = fh.readlines()
+        for line in lines0:
+            if line.startswith(">"):
+                id = line
+                fh2.write(id)
+            else:
+                seq = line
+                seq = seq.strip()
+                gc = str(GC(seq))
+                fh2.write(gc)
+                fh2.write("\n")
 
 #4
-
+with open('input.txt', 'r') as fh:
+    with open('output.txt', 'w') as fh2:
+        numlist = fh.read().split("\n")
+        sum = 0
+        for i in numlist:
+            i = int(i)
+            if i >= 0 and i%2 == 0:
+                sum+=i
+        sum = str(sum)
+        fh2.write(sum)
 #5
+
+
 
 #6
 
