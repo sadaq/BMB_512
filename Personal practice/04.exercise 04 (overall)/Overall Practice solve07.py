cae9bc134d2104375a87968f4d008f366eea9bcf
DESKTOP-2\sadaq <srmegh84@gmail.com>
2017-12-16 19:27:00 +0600

    no message

--- Personal practice/04.exercise 04 (overall)/Overall Practice solve07.py
+++ Personal practice/04.exercise 04 (overall)/Overall Practice solve07.py
@@ -1,11 +1,21 @@
 #Trivial
 
-#5
+#5 cpu time
+import time
+start = time.time()
+#program
+for i in range(100000):
+    pass
+end =time.time()
+
+print "time", (end-start)
 
 
-#6
-
-
-
-
-
+#6 random mutation
+import random
+seq = "ATGCATTGCATCGATCAACGTA"
+bases = 'ATGC'
+point = random.randint(0,len(seq))
+mutation = random.choice(bases)
+newseq = seq[:point] + mutation + seq[point+1:]
+print newseq
